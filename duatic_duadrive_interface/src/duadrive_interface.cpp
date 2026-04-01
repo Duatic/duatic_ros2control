2cb1768c8e528c0277c31dfe0dae87e7c37cad4b
Lennart Nachtigall <mail@firesurfer.de>
2026-04-01 14:51:31 +0200

    print the current state enum

--- duatic_duadrive_interface/src/duadrive_interface.cpp
+++ duatic_duadrive_interface/src/duadrive_interface.cpp
@@ -282,7 +282,8 @@ hardware_interface::return_type DuaDriveInterface::write([[maybe_unused]] const 
     // We always fill all command fields but depending on the mode only a subset is used
     drive_->setCommand(cmd);
   } else {
-    RCLCPP_ERROR_STREAM(logger_, get_name() << " Is not in target FSM Mode: ControlOP");
+    RCLCPP_ERROR_STREAM(logger_, get_name() << " Is not in target FSM Mode: ControlOP actual mode: "
+                                            << drive_->getActiveStateEnum());
   }
 
   // From this part of the drive API we do not get any feedback. Therefore we can only return OK here
