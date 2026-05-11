6ada84570938d01a9ee4ebf345632d6da28ee48f
Lennart Nachtigall <mail@firesurfer.de>
2026-05-11 16:48:19 +0200

    remove the part of the safety scaling which actually writes to the hardware as it created some issues

--- duatic_duadrive_interface/src/duadrive_interface.cpp
+++ duatic_duadrive_interface/src/duadrive_interface.cpp
@@ -307,23 +307,6 @@ hardware_interface::return_type DuaDriveInterface::write([[maybe_unused]] const 
 
     // We always fill all command fields but depending on the mode only a subset is used
     drive_->setCommand(cmd);
-
-    // Given the current scaling values calculate the maximum torque/maximum velocity values
-    const double new_max_torque = std::clamp(command_.scaling_factor_max_torque, 0.0, 1.0) * configured_max_torque_;
-    const double new_max_velocity =
-        std::clamp(command_.scaling_factor_max_velocity, 0.0, 1.0) * configured_max_velocity_ * configured_gear_ratio_;
-
-    // In case the changed enough -> perform an sdo write of the new maximum values
-
-    if (std::abs(new_max_torque - current_max_torque_) > 0.5) {
-      current_max_torque_ = new_max_torque;
-      drive_->setMaxJointTorque(current_max_torque_);
-    }
-    if (std::abs(new_max_velocity - current_max_velocity_) > 1.0) {
-      current_max_velocity_ = new_max_velocity;
-      drive_->setMaxMotorVelocity(current_max_velocity_);
-    }
-
   } else {
     rsl_drive_sdk::ReadingExtended reading;
     drive_->getReading(reading);
